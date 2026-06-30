5e75aebc3c82263119b273959fb90c271b3840c9
lousainachicken <Luziandre588@gmail.com>
2026-06-30 19:38:29 -0400

    Refactor brain properties into an object

--- Project-Mahoraga.js
+++ Project-Mahoraga.js
@@ -1,4 +1,9 @@
-brain.name = "Project-Mahoraga";
-brain.health = 100;
-brain.level = 1;
-brain.wins = 0;
+const brain = {
+    name: "Project-Mahoraga",
+    health: 100,
+    level: 1,
+    wins: 0,
+    attackScore: 0,
+    runScore: 0,
+    jumpScore: 0
+};
