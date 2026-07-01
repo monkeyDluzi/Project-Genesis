4fc210bff3955e7ede6f40165bf8744beb80b3d8
lousainachicken <Luziandre588@gmail.com>
2026-06-30 22:35:53 -0400

    Update enemy position and add movement logic

--- Project-Mahoraga.js
+++ Project-Mahoraga.js
@@ -7,13 +7,28 @@ const brain = {
     x: 100,
     y: 100,
 
-    enemyX: 0,
-    enemyY: 0,
+    enemyX: 300,
+    enemyY: 200,
     enemyHealth: 100,
     enemyAttackDamage: 25,
     enemyDamageTaken: 0
 };
 
+// make it visible on screen
 dragon.style.position = "absolute";
-dragon.style.left = brain.x + "px";
-dragon.style.top = brain.y + "px";
+
+function updateDragon() {
+    dragon.style.left = brain.x + "px";
+    dragon.style.top = brain.y + "px";
+}
+
+// movement logic
+function think() {
+    brain.x += 2; // moves right slowly
+}
+
+// game loop
+setInterval(() => {
+    think();
+    updateDragon();
+}, 30);
